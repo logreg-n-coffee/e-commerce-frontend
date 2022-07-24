32254ef6065e24b1cb49e0b12f107ee9046052ed
Rui Hu <8199515+logreg-n-coffee@users.noreply.github.com>
2022-07-24 02:45:02 -0700

    Globalize cart state using context

--- hooks/useCart.js
+++ hooks/useCart.js
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, createContext, useContext, useEffect } from "react";
+
+import { initiateCheckout } from "../lib/payment";
 
 import products from '../products.json';
 
@@ -6,11 +8,29 @@ const defaultCart = {
   products: {},
 };
 
-export default function useCart () {
+export const CartContext = createContext();
+
+export function useCartState () {
 
       const [cart, updateCart] = useState(defaultCart);
 
-      console.log("cart", cart);
+      // at the start of the refresh cycle, load the cart from local storage
+      useEffect(() => {
+        // local state from local storage
+        const stateFromLocalStorage = window.localStorage.getItem('savedCart');
+        // check if stateFromLocalStorage exists and parse the json file into a cart state object
+        const data = stateFromLocalStorage && JSON.parse(stateFromLocalStorage);
+        if ( data ) {
+          updateCart(data);
+        }
+      }, []);
+      
+      // save cart state to local storage every time when cart state changes
+      useEffect(() => {
+        const data = JSON.stringify(cart);
+        window.localStorage.setItem("savedCart", data);
+      }, [cart]);
+      
 
       const cartItems = Object.keys(cart.products).map((key) => {
         const product = products.find(({ id }) => `${id}` === `${key}`);
@@ -20,19 +40,14 @@ export default function useCart () {
         };
       });
 
-      console.log("cartItems", cartItems);
-
       const subtotal = cartItems.reduce((acc, { pricePerItem, quantity }) => {
         return acc + pricePerItem * quantity;
       }, 0);
-
-      console.log("subtotal", subtotal);
 
       const quantity = cartItems.reduce((acc, { quantity }) => {
         return acc + quantity;
       }, 0);
 
-      console.log("quantity", quantity);
 
       const addToCart = ({ id } = {}) => {
         updateCart((prev) => {
@@ -51,10 +66,37 @@ export default function useCart () {
         });
       };
 
+
+      const checkout = () => {
+        initiateCheckout({
+          // initiateCheckout({
+          //   lineItems: [
+          //     {
+          //       price: id,
+          //       quantity: 1,
+          //     },
+          //   ],
+          // });
+          lineItems: cartItems.map(({ id, quantity }) => {
+            return {
+              price: id,
+              quantity: quantity,
+            };
+          }),
+        });
+      };
+
       return {
         cart,
+        updateCart,
         subtotal,
         quantity,
-        addToCart
+        addToCart, 
+        checkout
       };
 }
+
+export const useCart = () => {
+  const cart = useContext(CartContext);
+  return cart;
+};
